2d7a45ff336eca98723427002661c912a71578bb
袁佳泉 <852732248@qq.com>
2017-12-29 10:12:24 +0800

    dd

--- string/stringtoint.cpp
+++ string/stringtoint.cpp
@@ -1,3 +1,81 @@
+#include <iostream>
+#include <string>
+#include <fstream> 
+#include <vector>
+using namespace std;
+string name = "C:\\Users\\HP\\Desktop\\11.txt";
+string result_name = "C:\\Users\\HP\\Desktop\\result.txt";
+fstream my_file;
+fstream result_file;
+
+
+int Char16ToInt(char c);
+int StrToNum16(const char* str)
+
+{
+	int len = strlen(str);
+	int num = 0;
+	for (int i = 0; i < len; i++)
+	{
+		num = num * 16 + Char16ToInt(str[i]);
+	}
+
+	return num;
+}
+int Char16ToInt(char c)
+{
+	switch (c)
+	{
+	case '0':
+		return 0;
+		break;
+	case '1':
+		return 1;
+		break;
+	case '2':
+		return 2;
+		break;
+	case '3':
+		return 3;
+		break;
+	case '4':
+		return 4;
+		break;
+	case '5':
+		return 5;
+		break;
+	case '6':
+		return 6;
+		break;
+	case '7':
+		return 7;
+		break;
+	case '8':
+		return 8;
+		break;
+	case '9':
+		return 9;
+		break;
+	case 'A':
+		return 10;
+		break;
+	case 'B':
+		return 11;
+		break;
+	case 'C':
+		return 12;
+		break;
+	case 'D':
+		return 13;
+		break;
+	case 'E':
+		return 14;
+		break;
+	case 'F':
+		return 15;
+		break;
+	default:
+		return 0;
 		break;
 
 	}
