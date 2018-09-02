be9a6753ecc71d8554e6e90e7170d79126ece9e4
袁佳泉 <852732248@qq.com>
2018-09-02 22:35:41 +0800

    -1

--- Astar/Astar/Astar.h
+++ Astar/Astar/Astar.h
@@ -8,6 +8,7 @@
 #include<vector>
 #include<iostream>
 
+
 using namespace std;
 struct Point
 {
@@ -36,9 +37,9 @@ struct Point
 class ASTAR
 {
 private:
-	const int OBLIQUE = 14;
+	const int OBLIQUE = 21; //只考虑上下左右
 	const int STEP = 10;
-	char maze[1000][1000];
+	int maze[100][100];
 	int rows;
 	int cols;
 	list<Point*> closeList;
@@ -48,11 +49,12 @@ private:
 		 if (x < 0 || x >= rows || y < 0 || y >= cols)
 			 return false;
 		 else 
-			return maze[x][y] == '0';
+			return maze[x][y] == 0; // 未访问过
 	}
 	 bool CanReach(Point* start, int x, int y, bool IsIgnoreCorner)
 	 {
 		 if (!CanReach(x, y) || findInList(closeList,x,y))
+		// if (!CanReach(x, y) )
 			 return false;
 		 else
 		 {
@@ -88,6 +90,22 @@ private:
 		 }
 		 return false;
 	 }
+	 bool findEndList(list<Point*> l, Point* p)
+	 {
+		 for (auto point : l)
+		 {
+			 if (point->x == p->x&&point->y == p->y)
+			 {
+				 p->F = point->F;
+				 p->G = point->G;
+				 p->H = point->H;
+				 p->parent = point->parent;
+				 return true;
+			 }
+		 }
+		 return false;
+	 }
+
 	 int CalcG(Point* start, Point* point)
 	 {
 		 int G = (abs(point->x - start->x) + abs(point->y - start->y)) == 1 ? STEP : OBLIQUE;
@@ -109,18 +127,24 @@ private:
 			 point->CalcF();
 		 }
 	 }
-
-	  void NotFoundPoint(Point* tempStart, Point* end, Point* point)
+	 
+	  void NotFoundPoint(Point* tempStart, Point* end, Point* point,bool dfs=false)
 	 {
 		 point->parent = tempStart;
+		 if (!dfs)
+		 {
+			 point->H = CalcH(end, point);
+		 }
+		 else
+			 point->H = 0;
+
 		 point->G = CalcG(tempStart, point);
-		 point->H = CalcH(end, point);
 		 point->CalcF();
 		 openList.push_front(point);
 	 }
 
 public:
-	ASTAR(char *a, int rows, int cols)
+	ASTAR(int *a, int rows, int cols)
 	{
 		this->cols = cols;
 		this->rows = rows;
@@ -136,7 +160,7 @@ public:
 	{
 		return a->F < b->F;
 	}
-	Point* FindPath(Point* start, Point* end, bool IsIgnoreCorner) 
+	Point* FindPath(Point* start, Point* end, bool IsIgnoreCorner,bool isDfs=false) 
 	{
 		openList.push_front(start);
 		while (!openList.empty())
@@ -146,44 +170,72 @@ public:
 			Point* tempStart = openList.front();
 			openList.pop_front();
 			closeList.push_front(tempStart);
+			if (tempStart->x == end->x && tempStart->y == end->y)
+			{
+				maze[tempStart->x][tempStart->y] = -1;
+				Point *root = tempStart;
+				while (root != NULL)
+				{
+					maze[root->x][root->y] = -1;
+					root = root->parent;
+				}
+				show();
+				cout << tempStart->F << endl;
+				return tempStart;
+			}
 
 			//经过点变换
-
-			maze[tempStart->x] [ tempStart->y] = '*';
+			maze[tempStart->x] [ tempStart->y] = 2;
 			show();
 	
-
 			//找到相邻点
 			list<Point*>surroundPoints = SurrroundPoints(tempStart, IsIgnoreCorner);
 			for(Point *point : surroundPoints)
 			{
 				//如果该点在列表中，更新G值
 				if (findInList(openList,point))
-					//计算G值, 如果比原来的大, 就什么都不做, 否则设置它的父节点为当前点,并更新G和F
-					FoundPoint(tempStart, point);
+					FoundPoint(tempStart, point); //计算G值, 如果比原来的大, 就什么都不做, 否则设置它的父节点为当前点,并更新G和F
+				else if (findInList(closeList, point))
+				{
+
+				}
 				else
 					//如果它们不在开始列表里, 就加入, 并设置父节点,并计算GHF
-					NotFoundPoint(tempStart, end, point);
+					NotFoundPoint(tempStart, end, point,isDfs);
+			}
+		}
+			return NULL;
+	}
+	Point* BFS(Point* start, Point* end, bool IsIgnoreCorner)
+	{
+		openList.push_front(start);
+		while (!openList.empty())
+		{
+			Point* tempStart = openList.front();
+			openList.pop_front();
+			closeList.push_front(tempStart);
+
+			//经过点变换
+			maze[tempStart->x][tempStart->y] = 2;
+			show();
+
+			//找到相邻点
+			list<Point*>surroundPoints = SurrroundPoints(tempStart, IsIgnoreCorner);
+			for (Point *point : surroundPoints)
+			{
+				//如果该点在列表中，更新G值
+				if (!findInList(openList, point))
+					openList.push_back(point);
 			}
 			if (findInList(openList, end))
 			{
-				maze[end->x][end->y] = '*';
+				maze[end->x][end->y] = 2;
 				show();
 				return openList.front();
 			}
 		}
-
-		if (findInList(openList, end))
-		{
-			maze[end->x][end->y] = '*';
-			show();
-			return openList.front();
-		}
-		else
-			return NULL;
+	
 	}
-
-
 	list<Point*> SurrroundPoints(Point* point, bool IsIgnoreCorner)
 	{
 		list<Point*> surroundPoints;
@@ -202,15 +254,19 @@ public:
 
 	void show()
 	{
+		system("cls");
+		
 		for (int i = 0; i < rows; i++)
 		{
 			for (int j = 0; j < cols; j++)
 			{
 				
-				if(maze[i][j]=='*')
+				if(maze[i][j]==2)
 					printf("●");
-				else if (maze[i][j] == '0')
+				else if (maze[i][j] == 0)
 					printf("○");
+				else if (maze[i][j] == -1)
+					printf("★");
 				else
 					printf("■");
 			}
--- Astar/Astar/main.cpp
+++ Astar/Astar/main.cpp
@@ -1,30 +1,63 @@
-#include <cstdlib>
+﻿#include <cstdlib>
 #include <cstdio>
 #include "Astar.h"
+#include<ctime>
+#include<fstream>
+#include<iostream>
+#define random(a,b) (rand()%(b-a+1)+a)
 
+using namespace std;
+
+const char* file = "C:\\Users\\HP\\Desktop\\map.txt";
+//create random map
+void createMap(int rows, int cols)
+{
+	fstream out;
+	out.open(file, ios::out);
+	out << rows << " " << cols << endl;
+	srand((unsigned)time(NULL));
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			out <<0<<" ";
+		}
+		out << endl;
+	}
+	out.close();
+}
 
 int main()
 {
-	char pBuff[] = {
-		'0','0','0','0','0','0','0',
-		'0','1','1','0','0','1','1',
-		'0','S','1','0','1','E','0',
-		'0','1','0','0','0','1','0',
-		'0','0','0','1','0','1','0'
-		
-	};
-	ASTAR test(pBuff,5,7);
-	Point *start=new Point(0, 0);
-	Point *end=new Point(2, 3);
+	//createMap(10, 10);
+	fstream in(file);
+	int rows, cols;
+	in >> rows >> cols;
+	int length = rows*cols;
+	int *a = new int[length] {0};
+	int data;
+	for (int i = 0; i < length; i++)
+	{
+		in >> data;
+		a[i] = data;
+	}
+	in.close();
+
+
+	ASTAR test(a,rows,cols);
+	Point *start=new Point(5, 0 );
+	Point *end=new Point(5,9 );
 	test.show();
 	cout << endl;
+	bool dfs = true;
 	Point *root=test.FindPath(start, end, false);
 	if (root == NULL)
 		cout << "cannot find\n";
 	else
 		cout << "you get it\n";
 
+	delete[]a;
 
-	getchar();
+	system("pause");
 	return 0;
 }
